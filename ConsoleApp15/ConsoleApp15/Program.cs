bd036215239073b19913e166c3a19efc0e159f4c
jihoon <jihoon4578@gmail.com>
2023-12-04 15:27:37 +0900

    2023.12.4) 가상 함수 원리

---
+++ ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+
+    internal class Program
+    {
+        static void Beacon(Terran terran)
+        {
+            terran.Recovery();
+        }
+
+        static void Main(string[] args)
+        {
+            Marine marine = new Marine();
+            Firebet firebet = new Firebet();
+            Ghost ghost = new Ghost();
+
+            marine.Health -= 10;
+            firebet.Health -= 10;
+            ghost.Health -= 10;
+
+            Beacon(marine);
+            Beacon(firebet);
+            Beacon(ghost);
+        }
+    }
+}
---
+++ ConsoleApp15/ConsoleApp15/Terran.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal abstract class Terran
+    {
+        protected int health;   //내부 데이터
+        protected int maxHealth;
+
+        public int Health   //외부 데이터
+        {
+            get { return health; }
+            set
+            {
+                if(maxHealth < value)
+                {
+                    return;
+                }
+
+                health = value;
+                Console.WriteLine("health의 값 : " + health);
+            }
+        }
+
+        public abstract void Recovery();
+    }
+}
